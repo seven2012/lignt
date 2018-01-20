9f938ad598c9d4362fcbf46e67593a353bc16b96
july77 <ljlpsy11@gmail.com>
2018-01-20 18:46:34 +0800

    pre-rem

--- static/script/index.js
+++ static/script/index.js
@@ -1,21 +1,23 @@
 
+
 $('.player').on('click', function () {
-    // console.log('play')
     $('#video').css('left', '50%').css('opacity', '1')
     $('.closed').css('opacity', '1')
-
-
-    $('#video').hover(function () {
-        $('#video')[0].play() // play() 和 pause() 是HTML5中video自带的API函数，哈哈，方便吧？
-    }, function () {
-        $('#video')[0].pause()
+    $('#video')[0].play()  
+    $('#video').click(function () {    
+        if (this.paused == false) {
+            this.pause();     
+        } else {
+            this.play();
+            
+        }
     })
-
-
 })
+
 $('.closed').on('click', function () {
     $('#video').css('left', '-100%').css('opacity', '1')
     $('.closed').css('opacity', '0')
     $('#video')[0].pause()
     $('#video')[0].load()
-})
+});
+
