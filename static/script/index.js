0e92e8f7bf85c16d03a044b6bbb638b5f6b6b71c
july77 <ljlpsy11@gmail.com>
2018-01-21 23:14:52 +0800

    light

--- static/script/index.js
+++ static/script/index.js
@@ -1,24 +1,23 @@
 
-
 $('.player').on('click', function () {
     $('#video').css('left', '50%').css('opacity', '1')
     $('.closed').css('opacity', '1')
-    $('#video')[0].play()  
-    $('#video').click(function () {    
-        if (this.paused == false) {
-            this.pause();     
-        } else {
-            this.play();
-            
-        }
-    })
+    $('#video')[0].play()
+})
+
+$('#video').click(function () {
+    if (this.paused) {
+        this.play();
+    } else {
+        this.pause();
+    }
 })
 
 $('.closed').on('click', function () {
     $('#video').css('left', '-100%').css('opacity', '1')
     $('.closed').css('opacity', '0')
-    $('#video')[0].pause()
     $('#video')[0].load()
+    $('#video')[0].pause()
 });
 
 
